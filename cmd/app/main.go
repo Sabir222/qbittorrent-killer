6e6615e958d6c0536588e97ae7f64de86ffea789
sabir222 <mrsabir4@gmail.com>
2026-02-10 11:15:42 +0100

    add main.go entry point

---
+++ cmd/app/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/Sabir222/torrent-at-home/data/descriptor"
+)
+
+const (
+	version = "1.0.0"
+	name    = "torrent-at-home"
+)
+
+func banner() {
+	fmt.Println(strings.Repeat("─", 50))
+	fmt.Printf("  %s v%s\n", name, version)
+	fmt.Println(strings.Repeat("─", 50))
+}
+
+func entry() {
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "usage: %s <torrent-file> <output-path>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	src := os.Args[1]
+	dst := os.Args[2]
+
+	banner()
+	log.SetFlags(log.Ldate | log.Ltime)
+
+	meta, err := descriptor.Open(src)
+	if err != nil {
+		log.Fatalf("failed to load torrent: %v", err)
+	}
+
+	fmt.Printf("\n[info] torrent: %s\n", meta.Name)
+	fmt.Printf("[info] size: %.2f MB\n", float64(meta.Length)/1024/1024)
+	fmt.Printf("[info] pieces: %d\n\n", len(meta.PieceHashes))
+
+	if err := meta.DownloadToFile(dst); err != nil {
+		log.Fatalf("transfer failed: %v", err)
+	}
+
+	fmt.Println("\n" + strings.Repeat("─", 50))
+	fmt.Println("[success] download complete!")
+	fmt.Printf("[output] %s\n", dst)
+	fmt.Println(strings.Repeat("─", 50))
+}
+
+func main() {
+	entry()
+}
